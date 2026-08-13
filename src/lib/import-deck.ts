4778ac6837b845703a5a4a99ff6f38e6426591d3
Mike <69970540+mikerb95@users.noreply.github.com>
2026-08-12 22:37:23 -0500

    fix: update slugify function to correctly handle diacritical marks

--- src/lib/import-deck.ts
+++ src/lib/import-deck.ts
@@ -84,7 +84,7 @@ export function slugify(value: string): string {
   return (
     value
       .normalize('NFD')
-      .replace(/[̀-ͯ]/g, '')
+      .replace(/[\u0300-\u036f]/g, '')
       .toLowerCase()
       .replace(/[^a-z0-9]+/g, '-')
       .replace(/^-+|-+$/g, '')
